c6fd745ae31ff34c5271224ec234b0f60dcbb245
MAXENCEWATRIN <maxence.watr@gmail.com>
2018-03-12 11:40:39 +0100

    Moteur
    enlever la classe moteur pour la refaire ensemble après

--- ModuleBts/src/java/com/brasRobot/controlleur/javaBeans.java
+++ ModuleBts/src/java/com/brasRobot/controlleur/javaBeans.java
@@ -1,5 +1,6 @@
 package com.brasRobot.controlleur;
-import com.brasRobot.JavaBeans.Moteur;
+//import com.brasRobot.JavaBeans.Moteur;
+
 import com.brasRobot.JavaBeans.Musicien;
 import com.brasRobot.ejb.javaBeansEjb;
 import com.brasRobot.JavaBeans.Preference;
@@ -8,67 +9,73 @@ import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+
 @Named
 @RequestScoped
 public class javaBeans {
+
     @EJB
-    javaBeansEjb javaEjb; 
-    Musicien musicien =new Musicien();
+    javaBeansEjb javaEjb;
+    Musicien musicien = new Musicien();
     Preference preference = new Preference();
-    Moteur moteur = new Moteur();
+    //Moteur moteur = new Moteur();
+
     public void addMessageCamera() {
         System.out.println("Dans la camera");
         //ejb
         javaEjb.addMessageCameraEjb(musicien.isCamera());
         //graphique
-        String messageCamera = musicien.isCamera()? "la camera est allumer" : "la camera est etteint";
+        String messageCamera = musicien.isCamera() ? "la camera est allumer" : "la camera est etteint";
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(messageCamera));
     }
+
     //message laser
     public void addMessageLaser() {
         System.out.println("Dans le laser");
         javaEjb.addMessageLaserEjb(musicien.isLaser());
-        String messageLaser = musicien.isLaser()? "le laser est allumer" : "le laser est etteint";
+        String messageLaser = musicien.isLaser() ? "le laser est allumer" : "le laser est etteint";
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(messageLaser));
     }
+
     //ajouter une preference
-    public void ajouterPreference(){
-        System.out.println("ajout de preference"); 
+    public void ajouterPreference() {
+        System.out.println("ajout de preference");
         javaEjb.ajouterPreferenceEjb(preference);
         System.out.println("ajouter preference réalisée avec succès");
     }
+
     //modifier le laser
-    public void ajouterMusicien(){
-        System.out.println("ajout de laser");  
+    public void ajouterMusicien() {
+        System.out.println("ajout de laser");
         javaEjb.ajouterMusicienEjb(musicien);
         System.out.println("ajouter laser réalisée avec succès");
-    }  
-    public void ajouterMoteur(){
-        System.out.println("ajout du moteur");  
-        javaEjb.ajouterMoteurEjb(moteur);
-        System.out.println("ajouter moteur réalisée avec succès");
     }
-    
-    
-    
-    
+//    public void ajouterMoteur(){
+//        System.out.println("ajout du moteur");  
+//        javaEjb.ajouterMoteurEjb(moteur);
+//        System.out.println("ajouter moteur réalisée avec succès");
+//    }
+
     //get-set
     public Musicien getMusicien() {
         return musicien;
     }
+
     public void setMusicien(Musicien musicien) {
         this.musicien = musicien;
     }
+
     public Preference getPreference() {
         return preference;
     }
+
     public void setPreference(Preference preference) {
         this.preference = preference;
     }
-    public Moteur getMoteur() {
-        return moteur;
-    }
-    public void setMoteur(Moteur moteur) {
-        this.moteur = moteur;
-    }    
+//    public Moteur getMoteur() {
+//        return moteur;
+//    }
+//    public void setMoteur(Moteur moteur) {
+//        this.moteur = moteur;
+//    }    
 }
